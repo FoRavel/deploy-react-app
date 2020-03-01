0da99072f444ba04ded50b7c03541c06d5950a52
Fanilo RAVELOSON <faniloraveloson@MacBook-Pro-de-Fanilo.local>
2020-03-01 20:19:27 +0100

    added prod base url

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: 'http://localhost:8000/api' || 'https://thawing-mountain-92047.herokuapp.com/api',
 })
 
 export const insertProduct = payload => api.post(`/product`, payload)
